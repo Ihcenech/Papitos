036ba8514fdfa7799e17bba3745938754e6fd5b2
Ihcenech <ihcene.ch@gmail.com>
2016-04-30 19:55:06 -0400

    One and only commit

---
+++ CardboardTest2/Assets/Cardboard/Scripts/EnemySpawn.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+	public GameObject EnemyBarrel; 
+
+	public GameObject Enemy; 
+
+	public float countdown = 150.0f;
+    private System.Random ran;
+    private bool objHit = false;
+
+    public GameObject flare;
+
+	//public GameObject temporaryBulletHandler;
+
+
+	public float forwardForce;
+
+	// Use this for initialization
+	void Start () {
+        ran = new System.Random();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//float time = 0.5f; 
+		countdown -= Time.deltaTime; 
+		if (countdown <= 0.0f) 
+		{
+			GameObject temporaryEnemyHandler;
+
+			temporaryEnemyHandler = Instantiate (Enemy, EnemyBarrel.transform.position, EnemyBarrel.transform.rotation) as GameObject;
+
+			temporaryEnemyHandler.transform.Rotate (Vector3.left * 90);
+
+            temporaryEnemyHandler.transform.position = new Vector3(ran.Next(20), ran.Next(20), 40);
+
+			Rigidbody temporaryRigidBody;
+			temporaryRigidBody = temporaryEnemyHandler.GetComponent<Rigidbody> ();
+
+			temporaryRigidBody.AddForce (transform.forward * forwardForce);
+
+            if (objHit)
+            {
+                Instantiate(flare, temporaryEnemyHandler.transform.position, temporaryEnemyHandler.transform.rotation);
+                objHit = false;
+            }
+
+			Destroy (temporaryEnemyHandler, 5.0f);
+
+            countdown = 0.05f;
+
+		}
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Yoooo");
+        objHit = true;
+    }
+
+
+
+}
---
+++ CardboardTest2/Assets/OnCockpitCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCockpitCollision : MonoBehaviour
+{
+    public GameObject exp; 
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("am i here?");
+        if (other.gameObject.CompareTag("Asteroid"))
+        {
+            Destroy(other.gameObject);
+            Instantiate(exp, other.gameObject.transform.position, other.gameObject.transform.rotation);
+        }
+
+    }
+}
